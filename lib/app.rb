bed6ae64f53b66a521d9710ee913805e677d8182
Miguel Flores Silverio <floresmigu3l@gmail.com>
2016-02-15 00:49:54 -0800

    Solution to ToyCity Report Generator

--- lib/app.rb
+++ lib/app.rb
@@ -4,7 +4,8 @@ file = File.read(path)
 products_hash = JSON.parse(file)
 
 # Print today's date
-
+date = Time.now
+puts date
 puts "                     _            _       "
 puts "                    | |          | |      "
 puts " _ __  _ __ ___   __| |_   _  ___| |_ ___ "
@@ -14,7 +15,7 @@ puts "| .__/|_|  \\___/ \\__,_|\\__,_|\\___|\\__|___/"
 puts "| |                                       "
 puts "|_|                                       "
 
-
+	items = products_hash['items']
 # For each product in the data set:
   # Print the name of the toy
   # Print the retail price of the toy
@@ -22,7 +23,24 @@ puts "|_|                                       "
   # Calculate and print the total amount of sales
   # Calculate and print the average price the toy sold for
   # Calculate and print the average discount (% or $) based off the average sales price
-
+	sales = 0
+	items.each do |item|
+		puts "Item: #{item["title"]}"
+		puts "Price: #{item["full-price"]}"
+		puts "Purchases: #{item["purchases"].length}"
+		purchases = item["purchases"]
+		purchases.each do |purchase|
+			sales += purchase["price"]
+		end
+		item["Sales"] = sales
+		puts "Sales: $#{sales}"
+		average = sales/purchases.length
+		puts "Average Price: $#{average}"
+		discount = item["full-price"].to_f - average
+		puts "Average Discount: $#{discount}"
+		sales = 0
+		puts "\n"
+	end
 
 	puts " _                         _     "
 	puts "| |                       | |    "
@@ -37,3 +55,25 @@ puts "|_|                                       "
   # Count and print the number of the brand's toys we stock
   # Calculate and print the average price of the brand's toys
   # Calculate and print the total revenue of all the brand's toy sales combined
+brands_data = Hash.new
+items.each do |item|
+	brand = item["brand"]
+	item_price = item["full-price"].to_f
+	if !brands_data.has_key?(brand)
+		brands_data[brand] = Hash.new
+		brands_data[brand]["count"] = 1
+		brands_data[brand]["price"] = item_price
+		brands_data[brand]["Sales"] = item["Sales"]
+	else
+		brands_data[brand]["count"] += 1
+		brands_data[brand]["price"] += item_price
+		brands_data[brand]["Sales"] += item["Sales"]
+	end
+end
+brands_data.each do |brand, value|
+	average = value["price"] / value["count"]
+	puts brand
+	puts "Avarage Price: $#{average.round(2)}"
+	puts "Revenue: $#{value["Sales"].round(2)}"
+	puts "\n"
+end
